37f3c8cdfaa3d18236eec98448b54fd3251a0884
koalaylj <koalaylj@gmail.com>
2014-01-20 21:39:27 +0800

    change cross dir
    change cross dir

--- Assets/Scripts/CrossPresenter.cs
+++ Assets/Scripts/CrossPresenter.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class CrossPresenter : MonoBehaviour {
 
-	private int _speed = -10;
+	private int _speed = 8;
 
-	// Use this for initialization
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 		transform.Rotate(Vector3.forward * Time.deltaTime * _speed);
 	}
