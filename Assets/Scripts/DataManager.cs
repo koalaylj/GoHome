34b16e273d3c2906f2023a15a8947b792c159ec6
LazyFish <koalaylj@gmail.com>
2014-09-03 23:57:20 +0800

    添加新资源，补充机关表格，设计机关机制。

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataManager
+{
+
+    private DataManager _instance;
+
+    public DataManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new DataManager();
+            }
+            return _instance;
+        }
+    }
+
+    private DataManager()
+    {
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/UI/LevelComponent.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelComponent : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _start;
+
+    [SerializeField]
+    private UISprite _background;
+
+    private int _index;
+
+    public int Index
+    {
+        get { return _index; }
+        set
+        {
+            _index = value;
+
+        }
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
