31dd9b2e0e40f6837b6a6fcc01bb99e0b038cd54
LazyFish <koalaylj@gmail.com>
2014-10-14 01:25:53 +0800

    第七关完成

---
+++ Assets/Scripts/Hurt/FlowerAttackHurt.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 发射子弹类机关
+/// </summary>
+public class FlowerAttackHurt : KHurt
+{
+    private Animator _anim;
+    private Transform _trans;
+    private BoxCollider2D _collider;
+    private SpriteRenderer _render;
+
+    private float _fireRate;    //开火频率
+
+    private float _timeCount = 0;
+    
+    void Start()
+    {
+        _collider = GetComponent<BoxCollider2D>();
+        _anim = GetComponent<Animator>();
+        _trans = this.transform;
+        _render = GetComponent<SpriteRenderer>();
+
+        _fireRate = Properties[0];
+    }
+
+    void Update()
+    {
+        //根据帧动画动态调整碰撞框大小
+        _collider.center = _render.sprite.bounds.center;
+        _collider.size = new Vector2(_render.sprite.bounds.size.x * 0.75f, _render.sprite.bounds.size.y);
+
+        _timeCount += Time.deltaTime;
+
+        if (_timeCount >= _fireRate)
+        {
+            _anim.SetTrigger("eat");
+            _timeCount = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Hurt/FlowerGunHurt.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 发射子弹类机关
+/// </summary>
+public class FlowerGunHurt : KHurt
+{
+    [SerializeField]
+    private BulletGenerator _bulletGenerator;
+
+    /// <summary>
+    /// 动画控制器
+    /// </summary>
+    private Animator _anim;
+
+    private Transform _trans;
+
+    private float _fireRate;    //开火频率
+    private float _bulletSpeed; //子弹速度
+    private float _delay;
+
+    private float _timeCount = 0;
+
+
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _trans = this.transform;
+        _fireRate = Properties[0];
+        _bulletSpeed = Properties[1];
+        _delay = Properties[2];
+    }
+
+    void Update()
+    {
+        _timeCount += Time.deltaTime;
+
+        if (_timeCount >= _fireRate + _delay)
+        {
+            _anim.SetTrigger("shoot");
+            _timeCount = 0;
+            _delay = 0;
+        }
+    }
+
+    void OnShoot()
+    {
+        _bulletGenerator.GenerateBullet(_bulletSpeed);
+    }
+}
