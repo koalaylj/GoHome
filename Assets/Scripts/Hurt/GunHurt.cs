871cea5bb8f8a67fbe5fba0209f542b5e0cd79b2
LazyFish <koalaylj@gmail.com>
2014-10-20 21:44:38 +0800

    第九关

--- Assets/Scripts/Hurt/GunHurt.cs
+++ Assets/Scripts/Hurt/GunHurt.cs
@@ -9,28 +9,49 @@ public class GunHurt : KHurt
     [SerializeField]
     private BulletGenerator _bulletGenerator;
 
+    /// <summary>
+    /// 动画控制器
+    /// </summary>
+    private Animator _anim;
+
     private Transform _trans;
 
     private float _fireRate;    //开火频率
     private float _bulletSpeed; //子弹速度
+    private float _delay;
 
     private float _timeCount = 0;
 
     void Start()
     {
+        _anim = GetComponent<Animator>();
         _trans = this.transform;
         _fireRate = Properties[0];
         _bulletSpeed = Properties[1];
+        _delay = Properties[2];
     }
 
     void Update()
     {
         _timeCount += Time.deltaTime;
 
-        if (_timeCount >= _fireRate)
+        if (_timeCount >= _fireRate + _delay)
         {
-            _bulletGenerator.GenerateBullet(_bulletSpeed);
+            if (_anim != null)
+            {
+                _anim.SetTrigger("shoot");
+            }
+            else
+            {
+                OnShoot();
+            }
             _timeCount = 0;
+            _delay = 0;
         }
+    }
+
+    void OnShoot()
+    {
+        _bulletGenerator.GenerateBullet(_bulletSpeed);
     }
 }
